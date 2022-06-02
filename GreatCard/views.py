c807253cf406e33337bd204c6617feda16d54233
abishek987 <abishekshah36@gmail.com>
2022-06-02 11:25:52 +0545

    first commit

---
+++ GreatCard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from store.models import Product
+def home(request):
+    products = Product.objects.all().filter(is_available = True)[0:7]
+    data = {
+        'products':products,
+    }
+    return render(request,'home.html',data)
---
+++ category/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Category
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name','cat_image', 'slug')
+    prepopulated_fields = {'slug': ('category_name',)}
+
+admin.site.register(Category,CategoryAdmin)
